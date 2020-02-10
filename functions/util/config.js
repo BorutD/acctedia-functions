1e177b4af3c9aaaf85a26c7c2e15b151b9f4d76c
BorutD <borut.drazumeric@gmail.com>
2020-02-10 10:27:47 +0100

    Added config details & installed firebase package.

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = config = {
+  apiKey: "AIzaSyDYJt0Ky_44mzsUE2wiP3sgCzAhBuSWT7M",
+  authDomain: "acctedia.firebaseapp.com",
+  databaseURL: "https://acctedia.firebaseio.com",
+  projectId: "acctedia",
+  storageBucket: "acctedia.appspot.com",
+  messagingSenderId: "823752295923",
+  appId: "1:823752295923:web:b734da55dcf7985bd8fe86"
+};
